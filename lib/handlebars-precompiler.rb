6905d8e9053c4112edc1605d318913b71fe68686
tchak <paul@chavard.net>
2012-10-31 20:40:53 +0100

    remove old files

--- lib/handlebars-precompiler.rb
+++
@@ -1,7 +0,0 @@
-require "handlebars-precompiler/version"
-
-module Handlebars
-  module Precompiler
-    # Your code goes here...
-  end
-end
--- lib/handlebars-precompiler/version.rb
+++
@@ -1,5 +0,0 @@
-module Handlebars
-  module Precompiler
-    VERSION = "0.0.1"
-  end
-end
