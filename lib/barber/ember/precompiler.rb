7e6d22a2d7cf6819b829e45ad8cea1f09e7eee59
tchak <paul@chavard.net>
2013-01-22 10:53:45 +0100

    use ember-template-compiler

--- lib/barber/ember/precompiler.rb
+++ lib/barber/ember/precompiler.rb
@@ -1,16 +1,12 @@
 module Barber
   module Ember
     class Precompiler < Barber::Precompiler
-      def ember
-        @ember ||= File.new(File.expand_path("../../javascripts/ember.js", __FILE__))
+      def handlebars
+        @ember ||= File.new(File.expand_path("../../javascripts/ember-template-compiler.js", __FILE__))
       end
 
       def precompiler
         @precompiler = File.new(File.expand_path("../../javascripts/ember_precompiler.js", __FILE__))
-      end
-
-      def sources
-        super << ember
       end
     end
   end
