e608610a4c9bbfe1cc0da4c463a4fdc9ddaeb4d4
Ryunosuke SATO <tricknotes.rs@gmail.com>
2014-12-27 19:15:48 +0900

    Support HTMLBars precompilation

--- test/ember/precompiler_test.rb
+++ test/ember/precompiler_test.rb
@@ -4,11 +4,11 @@ class EmberPrecompilerTest < MiniTest::Unit::TestCase
   def test_calls_the_ember_handlebars_precompiler
     result = compile "Hello {{name}}"
     assert result
-    assert_match /data\.buffer/, result
+    assert_match /data\.buffer|isHTMLBars: true/, result
   end
 
   def test_is_a_precompiler
-    assert Barber::Ember::Precompiler < Barber::Precompiler, 
+    assert Barber::Ember::Precompiler < Barber::Precompiler,
       "Ember precompile should inherit from Barber::Precompiler"
   end
 
