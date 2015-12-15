d4d6461852d11e5c89f261b2dac41946e3dad778
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-12-15 20:04:49 +0900

    Fix broken tests for 05e8d319

--- test/ember/precompilers_test.rb
+++ test/ember/precompilers_test.rb
@@ -5,14 +5,14 @@ class EmberPrecompilersTest < MiniTest::Unit::TestCase
   def compiled_template ; "function()" ; end
 
   def test_precompiles_inline_templates
-    Barber::Ember::Precompiler.expects(:compile).with(template).returns(compiled_template)
+    Barber::Ember::Precompiler.expects(:compile).with(template, nil).returns(compiled_template)
 
     result = Barber::Ember::InlinePrecompiler.call(template)
     assert_equal "Ember.Handlebars.template(#{compiled_template})", result
   end
 
   def test_precompiles_template_files
-    Barber::Ember::Precompiler.expects(:compile).with(template).returns(compiled_template)
+    Barber::Ember::Precompiler.expects(:compile).with(template, nil).returns(compiled_template)
 
     result = Barber::Ember::FilePrecompiler.call(template)
     assert_equal "Ember.Handlebars.template(#{compiled_template});", result
--- test/precompilers_test.rb
+++ test/precompilers_test.rb
@@ -5,14 +5,14 @@ class PrecompilersTest < MiniTest::Unit::TestCase
   def compiled_template ; "function()" ; end
 
   def test_precompiles_inline_templates
-    Barber::Precompiler.expects(:compile).with(template).returns(compiled_template)
+    Barber::Precompiler.expects(:compile).with(template, nil).returns(compiled_template)
 
     result = Barber::InlinePrecompiler.call(template)
     assert_equal "Handlebars.template(#{compiled_template})", result
   end
 
   def test_precompiles_template_files
-    Barber::Precompiler.expects(:compile).with(template).returns(compiled_template)
+    Barber::Precompiler.expects(:compile).with(template, nil).returns(compiled_template)
 
     result = Barber::FilePrecompiler.call(template)
     assert_equal "Handlebars.template(#{compiled_template});", result
